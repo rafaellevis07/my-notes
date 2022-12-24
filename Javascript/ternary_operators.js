c4e9c1450c316ed3b3a529e34a470e56cb050fce
Rafael Garcia <rafaellevis07@gmail.com>
2022-12-24 11:50:32 -0800

    ternary_operators done

--- Javascript/ternary_operators.js
+++ Javascript/ternary_operators.js
@@ -8,3 +8,34 @@ https://www.youtube.com/watch?v=m55PTVUrlnA&list=PLDBdmkfPal-zzfEv7GnbvvTiHmuYSi
 
 
 ================================================ Ternary Operators */
+
+/*This was created to minimize the lines of code in React specially
+
+
+Instead of a if statement */
+
+let age = 10;
+let name = "Pedro";
+
+if (age > 10) {
+  name = "Pedro";
+} else {
+  name = "Jack";
+}
+
+// We could use:
+
+let age1 = 16;
+let name1 = age1 > 10 ? "Rafael" : "Jack";
+
+// On react we would see this as:
+
+const Componet = () => {
+  return age1 > 10 ? <div> Pedro </div> : <div> Jack </div>;
+};
+
+/* Ternary operators
+    • And = &&
+    • Or = || 
+    • if/else operator = ? :
+*/
