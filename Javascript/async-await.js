5f1192a62ac7163e39677aa186558afbb92cfd29
Rafael Garcia <rafaellevis07@gmail.com>
2022-12-25 17:45:28 -0800

    update async

--- Javascript/async-await.js
+++ Javascript/async-await.js
@@ -8,7 +8,3 @@ https://www.youtube.com/watch?v=PgZ9npYJZzU
 
 
 ================================================ Async */
-var name = "Rafael";
-console.log(name);
-name = "John";
-console.log(name);
